6b5934af10bb8d469b0dbf67395ef3762b7590e0
Artyom <bergerromain151@gmail.com>
2020-03-25 12:39:22 +0100

    -Modifs map component

--- FrontEnd/src/components/map.js
+++ FrontEnd/src/components/map.js
@@ -3,12 +3,10 @@ import { render } from "react-dom";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-//import "leaflet/dist/leaflet.css";
-
 export default function MapInfos() {
-  const position = [44.859407, -0.584425];
+  const position = [50.665900, 4.612212];
   return (
-    <Map style={{ width: "70%", height: "300px" }} center={position} zoom={15}>
+    <Map style={{ width: "70%", height: "550px" }} center={position} zoom={18}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
