2dae611b83d57b13fb90901fc99acf5ece39f39a
Gilles Quirynen <gilles.quiry@gmail.com>
2020-03-25 13:06:13 +0100

    Revert "-Modifs map component"
    This reverts commit 6b5934af10bb8d469b0dbf67395ef3762b7590e0.

--- FrontEnd/src/components/map.js
+++ FrontEnd/src/components/map.js
@@ -3,10 +3,12 @@ import { render } from "react-dom";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+//import "leaflet/dist/leaflet.css";
+
 export default function MapInfos() {
-  const position = [50.665900, 4.612212];
+  const position = [44.859407, -0.584425];
   return (
-    <Map style={{ width: "70%", height: "550px" }} center={position} zoom={18}>
+    <Map style={{ width: "70%", height: "300px" }} center={position} zoom={15}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
