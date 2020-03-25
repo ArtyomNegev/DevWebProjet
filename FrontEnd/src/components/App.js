ce8297593a420b87e653e9a4f8842a7951150738
Artyom <bergerromain151@gmail.com>
2020-03-25 11:25:26 +0100

    Path fix

--- FrontEnd/src/components/App.js
+++ FrontEnd/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Routes from "./example_lio";
+import Routes from "./example";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "../theme/theme";
