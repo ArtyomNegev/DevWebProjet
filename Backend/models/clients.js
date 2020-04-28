ac993485f7cd7e16f09e3295116717c23a478849
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-04-28 14:22:52 +0200

    add password to Clients.js

--- Backend/models/clients.js
+++ Backend/models/clients.js
@@ -1,20 +1,28 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Clients = sequelize.define('Clients', {
-    userId: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-  },
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    numTel: DataTypes.STRING,
-    dateDeNaissance: DataTypes.DATE
-  }, {});
-  Clients.associate = function(models){
-    Clients.hasMany(models.Message,{
-      foreignKey : 'clientId',
-      as: 'message'
+  const Clients = sequelize.define(
+    "Clients",
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      numTel: DataTypes.STRING,
+      dateDeNaissance: DataTypes.DATE,
+    },
+    {}
+  );
+  Clients.associate = function (models) {
+    Clients.hasMany(models.Messages, {
+      foreignKey: "clientId",
+      as: "message",
     });
   };
   return Clients;
