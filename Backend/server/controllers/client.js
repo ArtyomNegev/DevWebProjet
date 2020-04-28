d53b8481b0196e5b61363c3ef6deb10a14b40a5d
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-04-28 14:20:48 +0200

    modifs : split client and message

--- Backend/server/controllers/client.js
+++ Backend/server/controllers/client.js
@@ -1,27 +1,18 @@
 const Client = require("../../models").Clients;
-const Message = require("../../models").Message;
+
 module.exports = {
   create(req, res) {
     return Client.create(
       {
         name: req.body.name + " " + req.body.firstName,
         email: req.body.email,
+        password: req.body.password,
         numTel: req.body.numTel,
-        dateDeNaissance: Date.parse(req.body.dateDeNaissance)
+        dateDeNaissance: Date.parse(req.body.dateDeNaissance),
       }
       // req.body)
     )
-      .then(client => {
-        return Message.create({
-          sujet: req.body.sujet,
-          contenu: req.body.contenu,
-          dateEnvoi: Date.now(),
-          clientId: client.userId
-        })
-          .then(message => res.status(201).send(message))
-          .catch(error => res.status(400).send(error));
-      })
-
-      .catch(error => res.status(400).send(error));
-  }
+      .then((message) => res.status(201).send(message))
+      .catch((error) => res.status(400).send(error));
+  },
 };
