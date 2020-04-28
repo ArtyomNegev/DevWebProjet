0865871b1f6bbc87be08c8a268873b5b76a994e9
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-04-28 22:34:52 +0200

    Update contact.js

--- FrontEnd/src/components/contact.js
+++ FrontEnd/src/components/contact.js
@@ -2,97 +2,60 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { CreateMessageAndClient } from "../http_calls/message";
+import { CreateMessage } from "../http_calls/message";
+import Grid from "@material-ui/core/Grid";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
       margin: theme.spacing(1),
-      width: "50%"
-    }
-  }
+      width: "50%",
+    },
+  },
 }));
 
 export default function Contact() {
-  const [Nom, setNom] = React.useState("");
-  const [Prenom, setPrenom] = React.useState("");
-  const [Email, setEmail] = React.useState("");
-  const [numTel, setnumTel] = React.useState("");
-  const [dateDeNaissance, setdateDeNaissance] = React.useState("");
   const [Sujet, setSujet] = React.useState("");
   const [Message, setMessage] = React.useState("");
   const classes = useStyles();
   const handleClick = () => {
-    CreateMessageAndClient(
-      Nom,
-      Prenom,
-      Email,
-      numTel,
-      dateDeNaissance,
-      Sujet,
-      Message
-    );
+    CreateMessage(Sujet, Message);
   };
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField
-        id="Nom"
-        label="Nom"
-        variant="outlined"
-        onChange={e => setNom(e.target.value)}
-      />
-      <br></br>
-      <TextField
-        id="Prenom"
-        label="Prénom"
-        variant="outlined"
-        onChange={e => setPrenom(e.target.value)}
-      />
-      <br></br>
-      <TextField
-        id="Email"
-        label="Email"
-        variant="outlined"
-        onChange={e => setEmail(e.target.value)}
-      />
-      <br></br>
-      <TextField
-        id="numTel"
-        label="Numéro de téléphone"
-        variant="outlined"
-        onChange={e => setnumTel(e.target.value)}
-      />
-      <br></br>
-      <TextField
-        id="date"
-        label="Date de naissance"
-        type="date"
-        variant="outlined"
-        onChange={e => setdateDeNaissance(e.target.value)}
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-      <TextField
-        id="Sujet"
-        label="Sujet"
-        variant="outlined"
-        onChange={e => setSujet(e.target.value)}
-      />
+    <Grid
+      container
+      spacing={3}
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item xs={12}>
+        <div align="center">
+          <h1 align="center">Contact</h1>
+          <br></br>
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+              id="Sujet"
+              label="Sujet"
+              variant="outlined"
+              onChange={(e) => setSujet(e.target.value)}
+            />
 
-      <TextField
-        id="Message"
-        label="Message"
-        multiline
-        rows="8"
-        fullWidth="true"
-        variant="outlined"
-        onChange={e => setMessage(e.target.value)}
-      />
-      <Button variant="contained" color="primary" onClick={handleClick}>
-        Envoyer
-      </Button>
-    </form>
+            <TextField
+              id="Message"
+              label="Message"
+              multiline
+              rows="8"
+              fullWidth="true"
+              variant="outlined"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button variant="contained" color="primary" onClick={handleClick}>
+              Envoyer
+            </Button>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
