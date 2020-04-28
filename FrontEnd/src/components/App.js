355796e2fc4d7483480b41cd45a61fcbcd6e76c1
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-04-28 22:34:06 +0200

    Update App.js

--- FrontEnd/src/components/App.js
+++ FrontEnd/src/components/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import Routes from "./example";
+import Routes from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "../theme/theme";
 import NavTabs from "./navtab";
 
-
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <NavTabs></NavTabs>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
 
 export default App;
-
