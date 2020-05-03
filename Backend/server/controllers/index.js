034fef3632262733f97ef97fb338ebec7d0dcbe3
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-05-03 10:32:25 +0200

    update index.js

--- Backend/server/controllers/index.js
+++ Backend/server/controllers/index.js
@@ -1,6 +1,8 @@
 const client = require("./client");
 const message = require("./message");
+const authenticate = require("./authenticate");
 module.exports = {
   client,
   message,
+  authenticate,
 };
