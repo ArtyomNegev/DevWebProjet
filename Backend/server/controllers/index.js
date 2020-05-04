56837b852a7ae7f9d214fe1169b3ba612cadb3b9
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-05-04 11:22:56 +0200

    update index.js

--- Backend/server/controllers/index.js
+++ Backend/server/controllers/index.js
@@ -1,8 +1,13 @@
 const client = require("./client");
 const message = require("./message");
 const authenticate = require("./authenticate");
+const appointment = require("./appointment");
+const calendar = require("./calendar");
+
 module.exports = {
   client,
   message,
   authenticate,
+  appointment,
+  calendar
 };
