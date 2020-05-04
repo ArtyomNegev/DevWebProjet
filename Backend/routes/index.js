d439498e31b2a997803f878d97378f79298242a6
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-05-04 11:21:50 +0200

    Update index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,6 +1,9 @@
 const controllers = require("../controllers");
 const messageController = controllers.message;
 const clientController = controllers.client;
+const appointmentController = controllers.appointment;
+const calendarController = controllers.calendar;
+
 const jwt = require("jsonwebtoken");
 const authenticateController = controllers.authenticate;
 const auth = require("../middleware/auth");
@@ -12,6 +15,17 @@ module.exports = (app) => {
     })
   );
   app.post("/api/client", clientController.create);
-  app.post("/api/message", auth, messageController.create);
+  
   app.post("/api/authenticate", authenticateController.login);
+
+  app.post("/api/message", auth, messageController.create);
+  app.post("/api/appointment", auth, appointmentController.create);
+
+  app.get("/api/appointments", auth, appointmentController.getCollection);
+  app.put("/api/appointments/:id", auth, appointmentController.update);
+  app.delete("/api/appointments/:id", auth, appointmentController.delete);
+
+  app.get("/api/moderator/appointments/", auth, appointmentController.getModeratorCollection);
+
+  app.get("/api/calendar/mondays", calendarController.getMondays);
 };
