d99b4862933e6f99a790983c557a5665d5c55a39
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-05-07 09:51:57 +0200

    create session.js

---
+++ FrontEnd/src/http_calls/session.js
@@ -0,0 +1,22 @@
+import { BASEURL } from "../constant/constant";
+import axios from "axios";
+
+export async function Authenticate(email, Mdp) {
+  return axios
+    .post(BASEURL + "api/authenticate", {
+      email,
+      Mdp,
+    })
+    .then(
+      (response) => {
+        console.log(response);
+        localStorage.setItem("JWTtoken", response.data.token);
+        localStorage.setItem("moderator", response.data.moderator);
+        console.log("put in localStorage");
+        console.log(localStorage.getItem("JWTtoken"));
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+}
