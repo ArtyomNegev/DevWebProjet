84b12f4402d040952824d545e0b238dc8ac48e6a
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-05-07 09:51:08 +0200

    update message.js

--- FrontEnd/src/http_calls/message.js
+++ FrontEnd/src/http_calls/message.js
@@ -1,22 +1,18 @@
 import { BASEURL } from "../constant/constant";
 import axios from "axios";
 
-export async function CreateMessageAndClient(
-  name,
-  firstName,
-  email,
-  numTel,
-  dateDeNaissance,
-  sujet,
-  contenu
-) {
-  return axios.post(BASEURL + "api/client", {
-    name,
-    firstName,
-    email,
-    numTel,
-    dateDeNaissance,
-    sujet,
-    contenu
-  });
+export async function CreateMessage(sujet, contenu) {
+  return axios.post(
+    BASEURL + "api/message",
+    {
+      sujet,
+      contenu,
+    },
+
+    {
+      headers: {
+        Authorization: localStorage.getItem("JWTtoken"),
+      },
+    }
+  );
 }
