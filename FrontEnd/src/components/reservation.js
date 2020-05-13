4f281896e1a7b9d7a2518f31d86af4c2e4ff3be8
Gilles Quirynen <44494786+VaelinAlSo@users.noreply.github.com>
2020-05-13 15:54:40 +0200

    Update reservation.js

--- FrontEnd/src/components/reservation.js
+++ FrontEnd/src/components/reservation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -11,23 +11,24 @@ import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import "../css/button.css";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { GetClientAppointments } from "../http_calls/clientAppointments";
 
-const StyledTableCell = withStyles(theme => ({
+const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
+    color: theme.palette.common.white,
   },
   body: {
-    fontSize: 14
-  }
+    fontSize: 14,
+  },
 }))(TableCell);
 
-const StyledTableRow = withStyles(theme => ({
+const StyledTableRow = withStyles((theme) => ({
   root: {
     "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.background.default
-    }
-  }
+      backgroundColor: theme.palette.background.default,
+    },
+  },
 }))(TableRow);
 
 function createData(
@@ -50,30 +51,77 @@ const rows = [
   createData("11h30-12h30", "", "", "", "", "", "", ""),
   createData("13h30-14h30", "", "", "", "", "", "", ""),
   createData("14h30-15h30", "", "", "", "", "", "", ""),
-  createData("15h30-16h30", "", "", "", "", "", "", "")
+  createData("15h30-16h30", "", "", "", "", "", "", ""),
 ];
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 500
-  }
+    minWidth: 500,
+  },
 });
 
 export default function Reservation() {
   const classes = useStyles();
 
+  const [recordedAppointments, setRecordedAppointments] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [currentPageDate, setCurrentPageDate] = useState(new Date(Date.now()).toISOString().split('T')[0]);
+
+	useEffect(() => {
+	    retrieveTutorials();
+	  }, []);
+
+	  const retrieveTutorials = () => {
+	    GetClientAppointments()
+	      .then(response => {
+	        setRecordedAppointments(response.data);
+	        // console.log(response.data);
+	      })
+	      .catch(e => {
+	        console.log(e);
+	      });
+	  };
+
+
+	const setActiveAppointment = (tutorial, index) => {
+	    //setCurrentTutorial(tutorial);
+	    setCurrentIndex(index);
+	  };
+
+
   return (
     <div>
       <h1>Réservation</h1>
+
+      <h2>Vos rendez-vous déj&agrave; enregistrés</h2>
+
+		<ul className="list-group">
+          {recordedAppointments &&
+            recordedAppointments.map((appointment, index) => (
+              <li
+                className={
+                  "list-group-item " + (index === currentIndex ? "active" : "")
+                }
+                onClick={() => setActiveAppointment(appointment, index)}
+                key={index}
+              >
+                le {appointment.date}, de {appointment.heureDebut} &agrave; {appointment.fin.split('T')[1].substring(0,5)}
+              </li>
+            ))}
+        </ul>
+
+
+      <hr/>
+
       <form className={classes.container} noValidate>
         <TextField
           id="date"
           label="Semaine du :"
           type="date"
-          defaultValue="2020-03-24"
+          defaultValue=  {currentPageDate}   // "2020-04-29"
           className={classes.textField}
           InputLabelProps={{
-            shrink: true
+            shrink: true,
           }}
         />
       </form>
@@ -92,50 +140,71 @@ export default function Reservation() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map(row => (
+            {rows.map((row) => (
               <StyledTableRow key={row.name}>
                 <StyledTableCell component="th" scope="row">
                   {row.horaire}
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   {row.lundi}
-                  <ButtonGroup aria-label="outlined primary button group" variant="contained">
+                  <ButtonGroup
+                    aria-label="outlined primary button group"
+                    variant="contained"
+                  >
                     <Button class="button"></Button>
                   </ButtonGroup>
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   {row.mardi}
-                  <ButtonGroup aria-label="outlined primary button group" variant="contained">
+                  <ButtonGroup
+                    aria-label="outlined primary button group"
+                    variant="contained"
+                  >
                     <Button class="button"></Button>
                   </ButtonGroup>
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   {row.mercredi}
-                  <ButtonGroup aria-label="outlined primary button group" variant="contained">
+                  <ButtonGroup
+                    aria-label="outlined primary button group"
+                    variant="contained"
+                  >
                     <Button class="button"></Button>
                   </ButtonGroup>
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   {row.jeudi}
-                  <ButtonGroup aria-label="outlined primary button group" variant="contained">
+                  <ButtonGroup
+                    aria-label="outlined primary button group"
+                    variant="contained"
+                  >
                     <Button class="button"></Button>
                   </ButtonGroup>
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   {row.vendredi}
-                  <ButtonGroup aria-label="outlined primary button group" variant="contained">
+                  <ButtonGroup
+                    aria-label="outlined primary button group"
+                    variant="contained"
+                  >
                     <Button class="button"></Button>
                   </ButtonGroup>
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   {row.samedi}
-                  <ButtonGroup aria-label="outlined primary button group" variant="contained">
+                  <ButtonGroup
+                    aria-label="outlined primary button group"
+                    variant="contained"
+                  >
                     <Button class="button"></Button>
                   </ButtonGroup>
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   {row.dimanche}
-                  <ButtonGroup aria-label="outlined primary button group" variant="contained">
+                  <ButtonGroup
+                    aria-label="outlined primary button group"
+                    variant="contained"
+                  >
                     <Button class="button"></Button>
                   </ButtonGroup>
                 </StyledTableCell>
