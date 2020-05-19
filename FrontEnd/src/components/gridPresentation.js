47241d8aa65a0f2ad6cebc47f7b1ce9f6fe4b4fb
Artyom <bergerromain151@gmail.com>
2020-05-19 19:08:53 +0200

    -Ajout des textes de l'onglet Présentation

--- FrontEnd/src/components/gridPresentation.js
+++ FrontEnd/src/components/gridPresentation.js
@@ -4,15 +4,15 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import logo from "../assets/logo.png";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
-    color: theme.palette.text.secondary
-  }
+    color: theme.palette.text.secondary,
+  },
 }));
 
 export default function GridPresentation() {
@@ -28,69 +28,73 @@ export default function GridPresentation() {
           <div id="LeftSideText">
             <p>
               <br></br>
-              Integer sit amet ipsum mollis, elementum lacus eu, iaculis enim.
-              Praesent in lacinia massa. Pellentesque posuere dolor porttitor,
-              laoreet orci eu, laoreet leo. Quisque pretium accumsan sapien vel
-              tincidunt. Cras efficitur erat id neque elementum aliquet.
-              Maecenas convallis justo orci, et dictum justo lobortis non.
-              <br></br> Nulla venenatis iaculis eleifend. Mauris dapibus magna
-              sed sapien ultricies bibendum. Nullam metus augue, vestibulum eu
-              urna vel, facilisis molestie eros. Ut vestibulum, augue ac
-              molestie vehicula, lorem quam viverra velit, lobortis varius eros
-              neque nec turpis.
+              Aujourd'hui, mes différentes expériences professionnelles m'ont
+              permis d'acquérir toutes les compétences nécessaires pour
+              intervenir auprès de bébés, d'enfants, d'adolescents que d'adultes
+              dont les difficultés et les souffrances psychiques ont un impact
+              sur le plan développemental, familial, affectif, somatique,
+              social, scolaire ou professionnel. Dans mon cabinet, je vous
+              propose une prise en charge adaptée à votre demande qui peut se
+              décliner sous forme d'entretiens psychologiques de soutien, d'un
+              accompagnement périnatal, d'une guidance parentale, de
+              consultations de couple ou familiales ou d'un entretien psycho
+              thérapeutique à plus long terme.
               <br></br>
-              Integer semper consequat massa in viverra. Phasellus sit amet
-              tempor urna. Phasellus accumsan, dui id lobortis lobortis, velit
-              nulla accumsan tortor, a posuere erat odio pulvinar nisi. Cras
-              dignissim purus in nisi condimentum luctus. Sed dictum neque in
-              odio faucibus, ac bibendum nisi convallis. Sed venenatis elementum
-              lorem, eget tristique sapien lacinia id. Aliquam erat volutpat.
-              Sed interdum lacus massa, non ultrices neque pharetra ut.
-              Phasellus nec eros tortor. Quisque ac efficitur elit. Mauris sed
-              tellus aliquam, mollis magna vel, cursus ipsum. Pellentesque
-              dapibus velit vitae odio tincidunt, a egestas nulla euismod. Donec
-              sodales commodo enim, eget lobortis libero venenatis vitae. Nunc
-              eget ultricies massa, at fringilla mi. Suspendisse aliquet eu mi
-              vitae molestie. Donec bibendum nulla ut erat vehicula, nec
-              consectetur massa consequat.
+              Mes spécialisations me permettent de réaliser des consultations
+              plus spécifiques en addictologie et en périnatalité. Mon objectif
+              est de vous accompagner vers le mieux être dans un cadre
+              chaleureux, accueillant et sécurisant. Attachée aux valeurs
+              déontologiques et éthiques, j'accorde une importance toute
+              particulière au respect du secret professionnel, à l'écoute et à
+              la compréhension de l'autre dans sa singularité.
             </p>
           </div>
         </Grid>
         <Grid item xs={6}>
-          <img alt="mental health" src={logo} width="400" height="400" id="RightSideImg"/>
+          <img
+            alt="mental health"
+            src={logo}
+            width="400"
+            height="400"
+            id="RightSideImg"
+          />
         </Grid>
         <Grid item xs={6}>
-          <img alt="mental health" src={logo} width="400" height="400" id="LeftSideImg"/>
+          <img
+            alt="mental health"
+            src={logo}
+            width="400"
+            height="400"
+            id="LeftSideImg"
+          />
         </Grid>
         <Grid item xs={6}>
           <div id="RightSideText">
             <p>
               <br></br>
-              Integer sit amet ipsum mollis, elementum lacus eu, iaculis enim.
-              Praesent in lacinia massa. Pellentesque posuere dolor porttitor,
-              laoreet orci eu, laoreet leo. Quisque pretium accumsan sapien vel
-              tincidunt. Cras efficitur erat id neque elementum aliquet.
-              Maecenas convallis justo orci, et dictum justo lobortis non.
-              <br></br> Nulla venenatis iaculis eleifend. Mauris dapibus magna
-              sed sapien ultricies bibendum. Nullam metus augue, vestibulum eu
-              urna vel, facilisis molestie eros. Ut vestibulum, augue ac
-              molestie vehicula, lorem quam viverra velit, lobortis varius eros
-              neque nec turpis.
+              Au sein de mon cabinet, je vous propose une écoute attentive, un
+              moment d'apaisement, de retour sur soi et de recherche de
+              solutions. Dans votre vie vous pouvez ressentir le besoin d'avoir
+              un moment pour vous, un temps pour échanger avec une personne
+              neutre sur vos ressentis, vos angoisses, vos craintes, vos
+              inquiétudes. Ces inquiétudes peuvent être très diverses :<br></br>
+              <br></br> • Vous traversez une période de bouleversement familial
+              ou personnel :maladie, décès, séparation, déménagement, perte
+              d'emploi, agression..
               <br></br>
-              Integer semper consequat massa in viverra. Phasellus sit amet
-              tempor urna. Phasellus accumsan, dui id lobortis lobortis, velit
-              nulla accumsan tortor, a posuere erat odio pulvinar nisi. Cras
-              dignissim purus in nisi condimentum luctus. Sed dictum neque in
-              odio faucibus, ac bibendum nisi convallis. Sed venenatis elementum
-              lorem, eget tristique sapien lacinia id. Aliquam erat volutpat.
-              Sed interdum lacus massa, non ultrices neque pharetra ut.
-              Phasellus nec eros tortor. Quisque ac efficitur elit. Mauris sed
-              tellus aliquam, mollis magna vel, cursus ipsum. Pellentesque
-              dapibus velit vitae odio tincidunt, a egestas nulla euismod. Donec
-              sodales commodo enim, eget lobortis libero venenatis vitae. Nunc
-              eget ultricies massa, at fringilla mi. Suspendisse aliquet eu mi
-              vitae molestie. Donec bibendum nulla ut erat vehicula, nec
-              consectetur massa consequat.
+              <br></br>• Vous êtes aux prises avec un sentiment de mal être :
+              anxiété, angoisse, tristesse, épuisement, troubles du sommeil,
+              somatisation...<br></br>
+              <br></br> • Vous connaissez des difficultés dans votre rapport à
+              une conduite ou un produit : troubles du comportement alimentaire,
+              phobies, addictions et dépendances... (Voir onglet addictions)
+              <br></br>
+              <br></br> • Vous allez être parent : craintes, angoisses,
+              questionnements... (Voir onglet périnatalité)
+              <br></br>
+              <br></br> • Vous connaissez des difficultés relationnelles avec
+              votre enfant : soutien à la parentalité Toutes ces raisons et
+              beaucoup d'autres sont valables et légitimes.
             </p>
           </div>
         </Grid>
